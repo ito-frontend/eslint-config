d735adcf05b433d0cbbd045ab498df69e0627f59
stevesun <stevesun@guoshipartners.com>
2024-09-18 09:35:30 +0800

    feat: Add check-file configuration to ESLint setup
    Introduces a new configuration for checking files in ESLint setup. 
    Includes options to enable or disable the check, specify ignored files, 
    and set naming conventions for file and folder names.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import antfu from '@antfu/eslint-config';
 
+import { getCheckFileConfig } from './configs/check-file.js';
 import { javascriptConfig } from './configs/javascript.js';
 import { stylelisticConfig } from './configs/stylistic.js';
 import { getTailwindConfig } from './configs/tailwindcss.js';
 import { vueConfig } from './configs/vue.js';
 
 const perfectionistSortConfig = [
-  {
-    rules: {
-      'perfectionist/sort-imports': [
-        'error',
-        {
-          newlinesBetween: 'always',
-        },
-      ],
-    },
-  },
+  // {
+  //   rules: {
+  //     'perfectionist/sort-imports': [
+  //       'error',
+  //       {
+  //         newlinesBetween: 'always',
+  //       },
+  //     ],
+  //   },
+  // },
 ];
 
 /**
- * Factory function to create an ITO configuration object.
+ * 工廠函數，用於創建各種框架和工具的 ESLint 設定物件。
  *
- * @param {object} options - The options for the configuration.
- * @param {string} options.framework - Which framework to use.
- * @param {number} options.vueVersion - The version of Vue.js to use.
- * @param {boolean} options.tailwind - Whether to include Tailwind CSS.
- * @param {boolean} options.typescript - Whether to include TypeScript.
- * @param {Array} options.otherConfigs - Additional configuration objects.
- * @returns {object} - The ITO configuration object.
+ * @param {object} options - 設定選項。
+ * @param {string} [options.framework] - 要使用的框架（'vue' 或 'react'）。
+ * @param {number} [options.vueVersion] - 要使用的 Vue 版本（如果框架是 'vue'）。
+ * @param {boolean} [options.tailwind] - 是否包含 Tailwind CSS 設定。
+ * @param {boolean} [options.typescript] - 是否包含 TypeScript 設定。
+ * @param {boolean | object} [options.checkFile] - 是否包含文件檢查設定。
+ * @param {boolean} [options.checkFile.enabled] - 啟用或禁用文件檢查設定的標誌。
+ * @param {string[]} [options.checkFile.ignores] - 要忽略的文件列表。
+ * @param {Array} [options.otherConfigs] - 要合併的其他設定。
+ * @returns {object} 生成的設定物件。
  */
 function itoConfigFactory(options) {
   const {
@@ -35,10 +39,12 @@ function itoConfigFactory(options) {
     vueVersion = 3,
     tailwind = false,
     typescript = true,
+    checkFile = false,
     otherConfigs = [],
   } = options || {};
 
   const tailwindConfig = getTailwindConfig(tailwind);
+  const checkFileConfig = getCheckFileConfig(checkFile);
 
   return antfu(
     {
@@ -63,6 +69,8 @@ function itoConfigFactory(options) {
     ...perfectionistSortConfig,
 
     ...tailwindConfig,
+
+    ...checkFileConfig,
 
     ...otherConfigs,
   );
